26c9f061443a514ad00ed54187f7a4335f9dd9da
hannibalbarx <vvarkey@riseup.net>
2014-10-21 17:14:48 +0530

    config file null handling

--- fast_solution.py
+++ fast_solution.py
@@ -38,8 +38,11 @@ test = parser.get('config', 'test_file')
 
 D = 2 ** parser.getint('config', 'D_exponent')  # number of weights use for each model, we have 32 of them
 alpha = .1   # learning rate for sgd optimization
-hash_cols = list(int(x) for x in parser.get('config', 'hash_cols').split(","))
-hash_joins = list(list(int(z) for z in y.split(",")) for y in list(x for x in parser.get('config', 'hash_joins').split(";")))
+
+hash_cols=list(int(x) for x in parser.get('config', 'hash_cols').split(",")) if parser.has_option('config', 'hash_cols') else []
+hash_joins = list(list(int(z) for z in y.split(",")) for y in list(x for x in parser.get('config', 'hash_joins').split(";"))) if parser.has_option('config', 'hash_joins') else []
+features_count = 146 + (len(hash_cols)*(len(hash_cols)-1)/2 if hash_cols else 0)+len(hash_joins)
+print 'features count = %s'%features_count
 
 # function, generator definitions ############################################
 
@@ -57,7 +60,7 @@ def data(path, label_path=None):
         if t == 0:
             # create a static x,
             # so we don't have to construct a new x for every instance
-            x = [0] * (146 + len(hash_cols)*(len(hash_cols)-1)/2+len(hash_joins))
+            x = [0] * (features_count)
             if label_path:
                 label = open(label_path)
                 label.readline()  # we don't need the headers
@@ -77,16 +80,19 @@ def data(path, label_path=None):
                 #       on different machines
                 x[m] = abs(hash(str(m) + '_' + feat)) % D
         tw = 145
-        for i in range(9):
-                for j in range(i+1,10):
-                        tw += 1
-                        x[tw] = abs(hash(row[hash_cols[i]]+"_x_"+row[hash_cols[j]])) % D
-	for i in range(len(hash_joins)):
-		join_str=""
-		for j in range(len(hash_joins[i])):
-			join_str+=row[hash_joins[i][j]]
-		tw += 1
-		x[tw] = abs(hash(join_str)) % D
+	if hash_cols:
+		for i in range(9):
+			for j in range(i+1,10):
+				tw += 1
+				x[tw] = abs(hash(row[hash_cols[i]]+"_x_"+row[hash_cols[j]])) % D
+	if hash_joins:			
+		for i in range(len(hash_joins)):
+			join_str=""
+			for j in range(len(hash_joins[i])-1):
+				join_str+=row[hash_joins[i][j]]+"_x_"
+			join_str+=row[hash_joins[i][-1]]
+			tw += 1
+			x[tw] = abs(hash(join_str)) % D
 
         # parse y, if provided
         if label_path:
