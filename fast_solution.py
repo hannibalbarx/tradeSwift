de80df12cdd15b7f374c668cc4502fb0954f8c00
hannibalbarx <vvarkey@riseup.net>
2014-10-23 11:49:15 +0530

    put back normal hash

--- fast_solution.py
+++ fast_solution.py
@@ -18,8 +18,6 @@ as the name is changed.
 from datetime import datetime
 from math import log, exp, sqrt
 
-from murmur import murmur3_x86_32
-
 import cPickle
 from ConfigParser import SafeConfigParser
 
@@ -91,14 +89,14 @@ def data(path, label_path=None):
                 # note, the build in hash(), although fast is not stable,
                 #       i.e., same value won't always have the same hash
                 #       on different machines
-                x[m] = abs(murmur3_x86_32(str(m) + '_' + feat)) % D
+                x[m] = abs(hash(str(m) + '_' + feat)) % D
         tw = 145
 	if deep_hash_joins:			
 		for i in range(len(deep_hash_joins)):
 			for j in range(len(deep_hash_joins[i])-1):
 				for k in range(j+1, len(deep_hash_joins[i])):
 					tw += 1
-					x[tw] = abs(murmur3_x86_32(str(tw)+"_"+row[deep_hash_joins[i][j]]+"_x_"+row[deep_hash_joins[i][k]])) % D
+					x[tw] = abs(hash(str(tw)+"_"+row[deep_hash_joins[i][j]]+"_x_"+row[deep_hash_joins[i][k]])) % D
 	if hash_joins:			
 		for i in range(len(hash_joins)):
 			join_str=""
@@ -106,7 +104,7 @@ def data(path, label_path=None):
 				join_str+=row[hash_joins[i][j]]+"_x_"
 			join_str+=row[hash_joins[i][-1]]
 			tw += 1
-                        x[tw] = abs(murmur3_x86_32(str(tw)+"_"+join_str)) % D
+                        x[tw] = abs(hash(str(tw)+"_"+join_str)) % D
 
         # parse y, if provided
         if label_path:
@@ -186,7 +184,6 @@ for ID, x, y in data(train, label):
     if ID % 100000 == 0:
         print('%s\tencountered: %d\tlogloss: %f' % (
             datetime.now(), ID, (loss/33.)/ID))
-cPickle.dump(w, open("weights.pickle","w"))
 
 if parser.has_option('config', 'validation_file'):
 	print 'validation...'
